446c2ed187f2f075d5c5c864e6bb439c3b6ecb80
SquarePants1991 <tomcat_money@126.com>
2017-04-27 10:50:01 +0800

    透视投影和正交投影

--- OpenGLESLearn/ViewController.m
+++ OpenGLESLearn/ViewController.m
@@ -23,15 +23,27 @@
 
 - (void)update {
     [super update];
-    float varyingFactor = sin(self.elapsedTime);
-    GLKMatrix4 scaleMatrix = GLKMatrix4MakeScale(varyingFactor, varyingFactor, 1.0);
-    GLKMatrix4 rotateMatrix = GLKMatrix4MakeRotation(varyingFactor , 0.0, 0.0, 1.0);
-    GLKMatrix4 translateMatrix = GLKMatrix4MakeTranslation(varyingFactor, 0.0, 0.0);
-    // transformMatrix = translateMatrix * rotateMatrix * scaleMatrix
-    // 矩阵会按照从右到左的顺序应用到position上。也就是先缩放（scale）,再旋转（rotate）,最后平移（translate）
-    // 如果这个顺序反过来，就完全不同了。从线性代数角度来讲，就是矩阵A乘以矩阵B不等于矩阵B乘以矩阵A。
+    float varyingFactor = self.elapsedTime;
+    
+    GLKMatrix4 rotateMatrix = GLKMatrix4MakeRotation(varyingFactor, 0, 1, 0);
+#define UsePerspective // 注释这行运行查看正交投影效果，解除注释运行查看透视投影效果
+#ifdef UsePerspective
+    // 透视投影
+    float aspect = self.view.frame.size.width / self.view.frame.size.height;
+    GLKMatrix4 perspectiveMatrix = GLKMatrix4MakePerspective(GLKMathDegreesToRadians(90), aspect, 0.1, 10.0);
+    GLKMatrix4 translateMatrix = GLKMatrix4MakeTranslation(0, 0, -1.6);
     self.transformMatrix = GLKMatrix4Multiply(translateMatrix, rotateMatrix);
-    self.transformMatrix = GLKMatrix4Multiply(self.transformMatrix, scaleMatrix);
+    self.transformMatrix = GLKMatrix4Multiply(perspectiveMatrix, self.transformMatrix);
+#else
+    // 正交投影
+    float viewWidth = self.view.frame.size.width;
+    float viewHeight = self.view.frame.size.height;
+    GLKMatrix4 orthMatrix = GLKMatrix4MakeOrtho(-viewWidth/2, viewWidth/2, -viewHeight / 2, viewHeight/2, -10, 10);
+    GLKMatrix4 scaleMatrix = GLKMatrix4MakeScale(200, 200, 200);
+    self.transformMatrix = GLKMatrix4Multiply(scaleMatrix, rotateMatrix);
+    self.transformMatrix = GLKMatrix4Multiply(orthMatrix, self.transformMatrix);
+#endif
+    
 }
 
 - (void)glkView:(GLKView *)view drawInRect:(CGRect)rect {
@@ -46,12 +58,12 @@
 #pragma mark - Draw Many Things
 - (void)drawTriangle {
     static GLfloat triangleData[36] = {
-        0,      0.5f,  0,  1,  0,  0, // x, y, z, r, g, b,每一行存储一个点的信息，位置和颜色
-        -0.5f,  0.0f,  0,  0,  1,  0,
-        0.5f,   0.0f,  0,  0,  0,  1,
-        0,      -0.5f,  0,  1,  0,  0,
-        -0.5f,  0.0f,  0,  0,  1,  0,
-        0.5f,   0.0f,  0,  0,  0,  1,
+        -0.5,   0.5f,  0,   1,  0,  0, // x, y, z, r, g, b,每一行存储一个点的信息，位置和颜色
+        -0.5f,  -0.5f,  0,  0,  1,  0,
+        0.5f,   -0.5f,  0,  0,  0,  1,
+        0.5,    -0.5f, 0,   0,  0,  1,
+        0.5f,  0.5f,  0,    0,  1,  0,
+        -0.5f,   0.5f,  0,  1,  0,  0,
     };
     [self bindAttribs:triangleData];
     glDrawArrays(GL_TRIANGLES, 0, 6);
