94911d21951eaa0b7c3e89b726c2a0b342c2bc06
SquarePants1991 <tomcat_money@126.com>
2017-05-11 16:02:09 +0800

    modify blend func

--- OpenGLESLearn/GLBaseViewController.m
+++ OpenGLESLearn/GLBaseViewController.m
@@ -63,7 +63,7 @@
     // 设置OpenGL状态
     glEnable(GL_DEPTH_TEST);
     glEnable(GL_BLEND);
-    glBlendFunc(GL_SRC_ALPHA, GL_DST_ALPHA);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 }
 
 #pragma mark - Update Delegate
